dd87998bbbe989e6e6e4b5155b9c18d00d5ec38a
lilulu <1051682777@qq.com>
2022-04-29 12:29:24 +0800

    Changes

--- src/com/company/ForDome1.java
+++ src/com/company/ForDome1.java
@@ -6,7 +6,7 @@ public class ForDome1 {
     public static void main(String[] args) {
         System.out.println("请输入大于100的三位数(如果您输入的是小数则只保留整数部分):");
         Scanner os = new Scanner(System.in);
-        int max = (int)os.nextDouble();
+        int max = (int) os.nextDouble();
         if (max > 100 && max <= 999) {
             for (int i = 100; i <= max; i++) {
                 int three = i / 10 / 10 % 10;
@@ -16,8 +16,7 @@ public class ForDome1 {
                     System.out.println("100至" + max + "期间的水仙花数为" + i);
                 }
             }
-        }
-        else
+        } else
             System.out.println("请输入符合要求的数字");
     }
 }
---
+++ src/com/company/WhileDome1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+
+public class WhileDome1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 30) {
+            System.out.println("循环次数" + i);
+            i++;
+        }
+    }
+}
