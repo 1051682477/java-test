3e6b5c74ea620783661fb1525fb619b5b49de487
lilulu <1051682777@qq.com>
2022-04-29 15:17:35 +0800

    Changes

---
+++ src/com/company/DoWhileDome.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class DoWhileDome {
+    public static void main(String[] args){
+        int x = 0;
+        do {
+            System.out.println("循环次数"+x);
+            x++;
+        }
+        while (x<20);
+    }
+}
---
+++ src/com/company/ForDome2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class ForDome2 {
+    public static void main(String[] args) {
+        int[] numbers = {20, 30, 40, 50, 10, 60};
+        for (int number : numbers) {
+            System.out.print(number + ",");
+        }
+        System.out.println();
+        String[] names = {"小明", "小刘", "金铭", "御清风"};
+        for (String name : names) {
+            System.out.print(name + " ");
+        }
+    }
+}
+
+
